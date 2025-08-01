97feddc0e447b9ba0c380043eb98dadf0bd8e04d
rinbarpen <lpoutyoumu@gmail.com>
2025-08-01 22:39:03 +0800

    feat(ssh): 添加自动生成SSH密钥对的客户端和服务器脚本
    添加两个脚本用于自动生成SSH密钥对：
    1. sshkey-autogen-client.sh 用于客户端生成密钥并复制到服务器
    2. sshkey-autogen-server.sh 用于服务器生成密钥并配置授权

---
+++ ssh/sshkey-autogen-client.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+name="$1"
+serv_user="$2"
+serv_ip="$3"
+if [ -z "$name" ] || [ -z "$serv_user" ] || [ -z "$serv_ip" ]; then
+  echo "用法: $0 <name> <serv_user> <serv_ip>"
+  exit 1
+fi
+
+targetMachine="4090-ubuntu2404"
+now=$(date +"%Y-%m-%d_%H-%M-%S")
+comment="$name $now"
+keyPath="$HOME/.ssh/ssh_${targetMachine}_${name}"
+
+ssh-keygen -t ed25519 -C "$comment" -f $keyPath
+
+mv "${keyPath}" "$HOME/.ssh/"
+ssh-copy-id "${keyPath}.pub" "${serv_user}@${serv_ip}"
---
+++ ssh/sshkey-autogen-server.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+name="$1"
+remote_user="$2"
+remote_ip="$3"
+if [ -z "$remote_user" ] || [ -z "$remote_ip" ]; then
+  disable_send=false
+else
+  disable_send=true
+fi
+
+if [ -z "$name" ]; then
+  echo "用法: $0 <name> [remote_user] [remote_ip]"
+  exit 1
+fi
+
+targetMachine="4090-ubuntu2404"
+now=$(date +"%Y-%m-%d_%H-%M-%S")
+comment="$name $now"
+keyPath="$HOME/.ssh/ssh_${targetMachine}_${name}"
+
+ssh-keygen -t ed25519 -C "$comment" -f $keyPath
+
+if [ "$disable_send" = false ]; then
+  mv "${keyPath}" ./
+else
+  scp "${keyPath}" "${remote_user}@${remote_ip}:~/.ssh/authorized_keys"
+fi
+cat "${keyPath}.pub" >> "$HOME/.ssh/authorized_keys"
